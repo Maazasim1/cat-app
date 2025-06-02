303f21f005a0aa449c2b0c4feb1bc4714a206ca3
maaz <mazzasimq1@gmail.com>
2025-06-02 20:19:36 +0500

    added new cat audio

--- Data/cats.js
+++ Data/cats.js
@@ -7,5 +7,103 @@ export const cats = [
     description: "Happy happy happy cat",
     slug: "happy"
   },
+  {
+    id: 'wiwiwi',
+    name: "Wi Wi Wi",
+    image: "/wiwiwi.jpg",
+    audio: "/audio/wiwiwi.mp3",
+    description: "wi wi wi cat",
+    slug: "wiwiwi"
+  },
+  {
+    id: 'awoo',
+    name: "Awoo",
+    image: "/awoo.jpg",
+    audio: "/audio/awoo.mp3",
+    description: "Awoo cat",
+    slug: "awoo"
+  },
+  {
+    id: 'sleepy',
+    name: "Sleepy",
+    image: "/sleepy.jpg",
+    audio: "/audio/sleepy.mp3",
+    description: "Sleepy cat",
+    slug: "Sleepy"
+  },
+  
+  {
+    id: 'Romantic',
+    name: "Romantic",
+    image: "/romantic.jpg",
+    audio: "/audio/romantic.mp3",
+    description: "Romantic cat",
+    slug: "Romantic"
+  },
+  {
+    id: 'sad',
+    name: "Sad",
+    image: "/sad.jpg",
+    audio: "/audio/sad.mp3",
+    description: "Sad cat",
+    slug: "Sad"
+  },
+  {
+    id: 'fart',
+    name: "Fart",
+    image: "/fart.jpg",
+    audio: "/audio/fart.mp3",
+    description: "Fart hamster",
+    slug: "Fart"
+  },
+  {
+    id: 'vomit',
+    name: "Vomit",
+    image: "/vomit.jpg",
+    audio: "/audio/vomit.mp3",
+    description: "Vomit cat",
+    slug: "vomit"
+  },
+  {
+    id: 'let-her-go',
+    name: "Let her go",
+    image: "/let-her-go.jpg",
+    audio: "/audio/let-her-go.mp3",
+    description: "Let her go cat",
+    slug: "let-her-go"
+  },
+  
+  {
+    id: 'chef',
+    name: "Chef",
+    image: "/chef.jpg",
+    audio: "/audio/chef.mp3",
+    description: "Chef cat",
+    slug: "chef"
+  },
+  {
+    id: 'dramatic',
+    name: "Dramatic",
+    image: "/dramatic.jpg",
+    audio: "/audio/dramatic.mp3",
+    description: "Dramatic cat",
+    slug: "dramatic"
+  },
+  {
+    id: 'hehe',
+    name: "Hehe",
+    image: "/hehe.jpg",
+    audio: "/audio/hehe.mp3",
+    description: "Hehe cat",
+    slug: "hehe"
+  },
+  {
+    id: 'huh',
+    name: "Huh",
+    image: "/huh.jpg",
+    audio: "/audio/huh.mp3",
+    description: "Huh cat",
+    slug: "huh"
+  },
   
 ];
